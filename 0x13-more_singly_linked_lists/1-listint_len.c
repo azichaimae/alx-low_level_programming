a4a2310488512ba0460e844f3593a94a5d801a8c
root <azichaimae20@gmail.com>
2023-05-02 09:14:42 -0700

    len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,25 +1,20 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- * Description: singly linked list node structure
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
  */
-typedef struct listint_s
+size_t listint_len(const listint_t *h)
 {
-        int n;
-        struct listint_s *next;
-} listint_t;
+ size_t num = 0;
 
-/* function prototypes */
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
+ while (h)
+ {
+ num++;
+ h = h->next;
+ }
 
-#endif
+ return (num);
+}
